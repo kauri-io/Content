6d15289940c290457802483d88b4b3b10e99d70c
Oleksandr Dodatko <dodikk88.reg@gmail.com>
2019-05-16 20:01:26 +0300

    [ru] [remix-ide] added link checker script runner

---
+++ remix-ide-your-first-smart-contract/check_links.ps1
@@ -0,0 +1,6 @@
+
+### for some reason globally installed npm package does not work for me on windows
+### ```npm install -g markdown-link-check```
+### installed locally as a workaround and added related files to ```.gitignore```
+#
+node .\node_modules\markdown-link-check\markdown-link-check --progress --verbose .\README.md
