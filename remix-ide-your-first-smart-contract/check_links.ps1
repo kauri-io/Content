7e35d3fe2f9b962d9f0a3611826ffded983ec740
Oleksandr Dodatko <dodikk88.reg@gmail.com>
2019-05-17 14:19:21 +0300

    [ru] [remix-ide] added more link checking scripts

--- remix-ide-your-first-smart-contract/check_links.ps1
+++ remix-ide-your-first-smart-contract/check_links.ps1
@@ -4,3 +4,9 @@
 ### installed locally as a workaround and added related files to ```.gitignore```
 #
 node .\node_modules\markdown-link-check\markdown-link-check --progress --verbose .\README.md
+
+node .\node_modules\markdown-link-check\markdown-link-check --progress --verbose ru-remix-ide-your-first-smart-contract.md
+
+
+node .\node_modules\markdown-link-check\markdown-link-check --progress --verbose en-remix-ide-your-first-smart-contract.md
+
